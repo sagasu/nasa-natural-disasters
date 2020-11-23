898aba618a9182f5bd37beaf59304cbd82b7d853
Mateusz Kopij <sagasu@gmail.com>
2020-11-23 21:01:32 +0000

    adding spinner and loading data from NASA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import Map from './components/map'
+import Loader from './components/loader'
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const {events} = await res.json();
+      
+      console.log(events);
+
+      setEventData(events);
+      setLoading(false);
+    }
+
+    fetchEvents();
+    console.log(eventData);
+  }, [])
+
   return (
     <div >
-      <Map />
+      {!loading ? <Map eventData={eventData} /> : <Loader/>}
     </div>
   );
 }
